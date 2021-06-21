6eee82fab3958f9d44f684263dd91e61d1516be8
Michael Sanderson <michael.sanderson@metoffice.gov.uk>
2021-06-21 10:43:15 +0100

    Code updated to Python 3

--- pydem/utils.py
+++ pydem/utils.py
@@ -32,14 +32,13 @@ Created on Tue Oct 14 17:25:50 2014
 
 @author: mpu
 """
-from geopy.distance import distance
 import os
-import gdal
 import osr
 import re
-from reader.gdal_reader import GdalReader
 
+import iris
 import numpy as np
+from Haversine import haversine
 from scipy.ndimage.filters import minimum_filter
 from scipy.ndimage.measurements import center_of_mass
 
@@ -64,14 +63,12 @@ def rename_files(files, name=None):
     The files are renamed in the same directory as the original file locations
     """
     for fil in files:
-        elev_file = GdalReader(file_name=fil)
-        elev, = elev_file.raster_layers
+        elev = iris.load_cube(fil)
         fn = get_fn(elev, name)
-        del elev_file
         del elev
         fn = os.path.join(os.path.split(fil)[0], fn)
         os.rename(fil, fn)
-        print "Renamed", fil, "to", fn
+        print("Renamed", fil, "to", fn)
 
 
 def parse_fn(fn):
@@ -102,7 +99,7 @@ def get_fn(elev, name=None):
 
     Parameters
     -----------
-    elev : GdalReader.raster_layer
+    elev : Iris cube
         A raster layer from the GdalReader object.
     name : str (optional)
         An optional suffix to the filename.
@@ -111,8 +108,17 @@ def get_fn(elev, name=None):
     fn : str
         The standard <filename>_<name>.tif with suffix (if supplied)
     """
-    gcs = elev.grid_coordinates
-    coords = [gcs.LLC.lat, gcs.LLC.lon, gcs.URC.lat, gcs.URC.lon]
+    if not elev.coords('latitude').has_bounds():
+        elev.coords('latitude').guess_bounds()
+    if not elev.coords('longitude').has_bounds():
+        elev.coords('longitude').guess_bounds()
+
+    LLC_lat = np.min(elev.coords('latitude').bounds)
+    URC_lat = np.max(elev.coords('latitude').bounds)
+    LLC_lon = np.min(elev.coords('longitude').bounds)
+    URC_lon = np.max(elev.coords('longitude').bounds)
+    coords = [LLC_lat, LLC_lon, URC_lat, URC_lon]
+
     return get_fn_from_coords(coords, name)
 
 
@@ -135,64 +141,27 @@ def get_fn_from_coords(coords, name=None):
     EW1 = ["W", "E"][coords[1] > 0]
     NS2 = ["S", "N"][coords[2] > 0]
     EW2 = ["W", "E"][coords[3] > 0]
-    new_name = "%s%0.3g%s%0.3g_%s%0.3g%s%0.3g" % \
-        (NS1, coords[0], EW1, coords[1], NS2, coords[2], EW2, coords[3])
+    new_name = "{}{:.3g}{}{:.3g}_{}{:.3g}{}{:.3g}".format( \
+        NS1, coords[0], EW1, coords[1], NS2, coords[2], EW2, coords[3])
     if name is not None:
         new_name += '_' + name
-    return new_name.replace('.', 'o') + '.tif'
+
+    return new_name.replace('.', 'o') + '.nc'
 
 
-def mk_dx_dy_from_geotif_layer(geotif):
+def mk_dx_dy_from_latlon(lat, lon):
     """
-    Extracts the change in x and y coordinates from the geotiff file. Presently
-    only supports WGS-84 files.
+    Extracts the change in x and y coordinates from the latitude and longitudes supplied.
+    Uses the Haversine formula to calculate the distances.
     """
-    ELLIPSOID_MAP = {'WGS84': 'WGS-84'}
-    ellipsoid = ELLIPSOID_MAP[geotif.grid_coordinates.wkt]
-    d = distance(ellipsoid=ellipsoid)
-    dx = geotif.grid_coordinates.x_axis
-    dy = geotif.grid_coordinates.y_axis
-    dX = np.zeros((dy.shape[0]-1))
-    for j in xrange(len(dX)):
-        dX[j] = d.measure((dy[j+1], dx[1]), (dy[j+1], dx[0])) * 1000  # km2m
-    dY = np.zeros((dy.shape[0]-1))
-    for i in xrange(len(dY)):
-        dY[i] = d.measure((dy[i], 0), (dy[i+1], 0)) * 1000  # km2m
+    dX = np.zeros((lat.shape[0]-1))
+    for j in list(range(len(dX))):
+        dX[j] = haversine((lat[j+1], lon[1]), (lat[j+1], lon[0])) * 1000  # km2m
+    dY = np.zeros((lat.shape[0]-1))
+    for i in list(range(len(dY))):
+        dY[i] = haversine((lat[i], lon[0]), (lat[i+1], lon[0])) * 1000  # km2m
+
     return dX, dY
-
-
-def mk_geotiff_obj(raster, fn, bands=1, gdal_data_type=gdal.GDT_Float32,
-                   lat=[46, 45], lon=[-73, -72]):
-    """
-    Creates a new geotiff file objects using the WGS84 coordinate system, saves
-    it to disk, and returns a handle to the python file object and driver
-
-    Parameters
-    ------------
-    raster : array
-        Numpy array of the raster data to be added to the object
-    fn : str
-        Name of the geotiff file
-    bands : int (optional)
-        See :py:func:`gdal.GetDriverByName('Gtiff').Create
-    gdal_data : gdal.GDT_<type>
-        Gdal data type (see gdal.GDT_...)
-    lat : list
-        northern lat, southern lat
-    lon : list
-        [western lon, eastern lon]
-    """
-    NNi, NNj = raster.shape
-    driver = gdal.GetDriverByName('GTiff')
-    obj = driver.Create(fn, NNj, NNi, bands, gdal_data_type)
-    pixel_height = -np.abs(lat[0] - lat[1]) / (NNi - 1.0)
-    pixel_width = np.abs(lon[0] - lon[1]) / (NNj - 1.0)
-    obj.SetGeoTransform([lon[0], pixel_width, 0, lat[0], 0, pixel_height])
-    srs = osr.SpatialReference()
-    srs.SetWellKnownGeogCS('WGS84')
-    obj.SetProjection(srs.ExportToWkt())
-    obj.GetRasterBand(1).WriteArray(raster)
-    return obj, driver
 
 
 def sortrows(a, i=0, index_out=False, recurse=True):
@@ -247,7 +216,7 @@ def sortrows(a, i=0, index_out=False, recurse=True):
     if recurse & (len(a[0]) > i + 1):
         for b in np.unique(a[:, i]):
             ids = a[:, i] == b
-            colids = range(i) + range(i+1, len(a[0]))
+            colids = list(range(i)) + list(range(i+1, len(a[0])))
             a[np.ix_(ids, colids)], I2 = sortrows(a[np.ix_(ids, colids)],
                                                   0, True, True)
             I[ids] = I[np.nonzero(ids)[0][I2]]
